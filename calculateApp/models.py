2a6756349b106876cd03d9f03b2c784f2c041d3a
YohaanS <yohaan.singla@gmail.com>
2024-06-25 08:49:11 -0700

    Maded more models for projects

--- calculateApp/models.py
+++ calculateApp/models.py
@@ -6,4 +6,32 @@ class login(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     description = models.TextField(null=False, blank=True)
-    
+
+Operations = {
+    ("Addition", "+"),
+    ("Subtraction", "-"),
+    ("Multiplication", "*"),
+    ("Division", "/")
+}
+
+class Calculate(models.Model):
+    operation = models.CharField(max_length=15,
+    choices=Operations, 
+    blank=False, 
+    null=False)
+    firstnumber = models.DecimalField(max_digits=30, decimal_places=25)
+    secondnumber = models.DecimalField(max_digits=30, decimal_places=25)
+
+Sodas = {
+    ("Pepsi", "pepsi"),
+    ("Mountain Dew", "mountain-dew"),
+    ("Coca Cola", "coca-cola"),
+    ("Limca", "limca"),
+    ("Sprite", "sprite"),
+    ("Fanta", "fanta")
+}
+
+class VendingMachine(models.Model):
+    amount = models.IntegerField()
+    soda = models.CharField(max_length=15, choices=Sodas)
+
