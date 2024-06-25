c2b0218df90646e5e850020b0806adb212845a33
YohaanS <yohaan.singla@gmail.com>
2024-06-24 18:43:26 -0700

    Database setup complete

--- calculateApp/models.py
+++ calculateApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class login(models.Model):
+    username = models.CharField(max_length=55)
+    password = models.CharField(max_length=75)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    description = models.TextField(null=False, blank=True)
+    
--- djangocalculator/settings.py
+++ djangocalculator/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'djangocalculator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoproj',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'yohaan1279'
     }
 }
 
